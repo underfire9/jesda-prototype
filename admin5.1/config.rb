87587da395b7a2ea08e546aa82cc15120e3bbbec
fire <underfire9@gmail.com>
2026-06-25 17:46:45 +0800

    sync all

---
+++ admin5.1/config.rb
@@ -0,0 +1,30 @@
+# Require any additional compass plugins here.
+
+http_path = "/"
+css_dir = "stylesheets"
+sass_dir = "sass"
+images_dir = "assets"
+javascripts_dir = "javascripts"
+
+relative_assets = false
+sass_options = {:debug_info=>false}
+line_comments = false
+preferred_syntax = :scss
+output_style = :expanded
+
+
+css_dir = "stylesheets" # by Fire.app 
+sass_dir = "sass" # by Fire.app 
+images_dir = "assets" # by Fire.app 
+javascripts_dir = "javascripts" # by Fire.app 
+fireapp_coffeescripts_dir = "coffeescripts" # by Fire.app 
+fireapp_livescripts_dir = "livescripts" # by Fire.app 
+fireapp_minifyjs_on_build = false # by Fire.app 
+fireapp_always_report_on_build = true # by Fire.app 
+output_style = :expanded # by Fire.app 
+relative_assets = false # by Fire.app 
+line_comments = false # by Fire.app 
+sass_options = {:debug_info=>false} # by Fire.app 
+sourcemap = true # by Fire.app 
+fireapp_coffeescript_options = {:bare=>false} # by Fire.app 
+fireapp_livescript_options = {:bare=>false} # by Fire.app 
